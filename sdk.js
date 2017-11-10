397be2ee4181c5f27d83078192157940cd92f496
MaximRobota <Maxim1106@mail.ru>
2017-11-10 12:05:18 +0200

    rel

--- sdk.js
+++ sdk.js
@@ -1,28 +1,22 @@
 (function () {
-    this.max = {
+    this.Widgets = {
         require: function (widgetName) {
             switch(widgetName){
                 case 'Widget_1':
                     return this.Widget_1 = function Widget_1 (object) {
-                        this.id = object.id;
-                        this.title = object.title;
                         this.byId = object.byId;
                         this.width = object.width;
                         this.height = object.height;
-                        this.background = object.background;
-                        this.iframe = `<iframe src="./templates/Widget_1.html" width="` + this.width + `" height="` + this.height+ `" align="left"></iframe>`;
+                        this.iframe = `<iframe src="./Widgets/Widgets_1/Widget_1.html" width="` + this.width + `" height="` + this.height+ `" align="left"></iframe>`;
                     };
 
                     break;
                 case 'Widget_2':
                     return this.Widget_2 = function Widget_1 (object) {
-                        this.id = object.id;
-                        this.title = object.title;
                         this.byId = object.byId;
                         this.width = object.width;
                         this.height = object.height;
-                        this.background = object.background;
-                        this.iframe = `<iframe src="./templates/Widget_2.html" width="` + this.width + `" height="` + this.height+ `" align="left"></iframe>`;
+                        this.iframe = `<iframe src="./Widgets/Widgets_2/Widget_2.html" width="` + this.width + `" height="` + this.height+ `" align="left"></iframe>`;
                     };
                     break;
                 default:
